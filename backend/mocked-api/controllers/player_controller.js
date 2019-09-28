0057c6e774652fc0521118151041f283a6eec6fd
Wesley Monte <wesley.monte@ccc.ufcg.edu.br>
2019-09-28 11:13:03 -0300

    Added await to signup method

--- backend/mocked-api/controllers/player_controller.js
+++ backend/mocked-api/controllers/player_controller.js
@@ -6,27 +6,25 @@ const httpStatusCode = require('../constants/http-status-code.json');
 
 const user_exists_msg = "User already exists";
 
-const signup = (req, res) =>{
+const signup = async (req, res) =>{
 	const { email, nick } = req.body.player;
 
-	players.find({$or : [{email:email}, {nick:nick}]})
-		.exec(function(err, result) {
-			if(err) console.error(err);
-			if (result) {
-				return res.status(httpStatusCode.BAD_REQUEST).send({ error: user_exists_msg}).end();
-			} else {
-				players.create(req.body.player)
-				.then(({id}) => {
-					console.log(id)
-					const token = jwt.sign({ id: id}, config.jwt_secret,{expiresIn: '1h'});
-					return res.send({token})
-				})
-				.catch(err => {
-					console.error(err);
-					return res.status(httpStatusCode.BAD_REQUEST).send({error: "Registration failed"});
-				})
-			}
-		})
+	p = await players.find({$or : [{email:email}, {nick:nick}]}).exec()
+	
+	if(p.length == 0){
+		players.create(req.body.player)
+			.then(({id}) => {
+				console.log("Id from player [" + req.body.player.nick + "]: " + id)
+				const token = jwt.sign({ id: id}, config.jwt_secret,{expiresIn: '1h'});
+				return res.send({token})
+			})
+			.catch(err => {
+				console.error(err);
+				return res.status(httpStatusCode.BAD_REQUEST).send({error: "Registration failed"});
+			})
+	} else {
+		res.status(httpStatusCode.BAD_REQUEST).send({ error: user_exists_msg}).end();
+	}
 }
 
 const routes = () => {
