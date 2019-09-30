916d5f2c2874a11b3f9e4b833e0a374bb6d25109
Anderson Dantas <a.kleber.d@gmail.com>
2019-09-29 23:30:00 -0300

    Implemented photo upload feature

--- backend/mocked-api/controllers/player_controller.js
+++ backend/mocked-api/controllers/player_controller.js
@@ -1,4 +1,6 @@
 const { Router } = require('express');
+const fileUpload = require('express-fileupload');
+const cloudinary = require('cloudinary').v2;
 const players = require('../models/Player');
 const jwt = require('jsonwebtoken');
 const config = require('../config');
@@ -8,6 +10,18 @@ const user_exists_msg = "User already exists";
 const user_not_found_msg = "User not found";
 const invalid_field_msg = "Please provide a valid username and password.";
 
+const router = Router();
+
+router.use(fileUpload({
+	useTempFiles : true,
+	tempFileDir : '/tmp/'
+}));
+
+cloudinary.config({
+  cloud_name: config.cloudinary.cloud_name,
+  api_key: config.cloudinary.api_key,
+  api_secret: config.cloudinary.api_secret
+})
 
 const signup = async (req, res) =>{
 	const { email, nick } = req.body.player;
@@ -77,11 +91,31 @@ const myProfile = async (req, res) => {
 	res.send(player.toProfile());
 }
 
-const routes = () => {
-    const router = Router();
+const imageUpload = async (req, res) => {
+	console.log('uploading...');
+	const { nick } = req.params;
+	const player = await players.findOne({ nick });
+	
+	if(!player)
+		return res.status(400).send({ error: user_not_found_msg});
+	
+	const { image } = req.files;
+	console.log('image', image);
+	try {
+		const result = await cloudinary.uploader.upload(image.tempFilePath);
+		player.photo = result.url;
+		player.save();
+		return res.send({url: result.url }).end();
+	} catch(e) {
+		console.log(e);
+		res.status(500).send({foo: 'bar - deu ruim'}).end();
+	}
+}
 
+const routes = () => {
     router.post('/signup', signup);
 		router.post('/login', login);
+		router.post('/:nick/image', imageUpload);
 		router.get('/:nick/me', myProfile);
 		router.get('/:nick', profile);
     router.all('*', (req, res) => res.status(404).send('Not Found'));
