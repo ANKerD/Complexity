8a088a205f959b4821a8a7ac09032b71559d692b
JonathanAllisson <jonathan.allisson.silva@ccc.ufcg.edu.br>
2019-10-13 02:19:46 -0300

    few corrections

--- backend/mocked-api/controllers/player_controller.js
+++ backend/mocked-api/controllers/player_controller.js
@@ -175,20 +175,24 @@ const removeFriend = async (req, res) => {
 }
 
 const forgetPassword = async (req, res) =>{
+	try{
 	const {email} = req.body.player;
 	const player = await Player.findOne({ email });
 	if(!player)
-		return res.status(400).send({user_not_found_msg});
+		return res.status(404).send({user_not_found_msg});
 	const {nick} = player;
 	const new_password =  await player.generateNewPassword();
 	emailT.send(mailer.forgetPass(email, nick, new_password));
 	return res.status(200).send({message: 'new password sent to email'});
+	}catch(e){
+		return res.status(500).send(e).end();
+	}
 	
 }
 
 
 const routes = () => {
-	router.post('/fpassword', forgetPassword);
+	router.post('/forgetpassword', forgetPassword);
   	router.post('/signup', signup);
 	router.post('/login', login);
 	router.get('/me', auth, myProfile);
