f8b84e7876a66f6357cb8f12932f29e53a085bee
Anderson Dantas <a.kleber.d@gmail.com>
2019-11-14 13:38:10 -0300

    Formatar

--- backend/mocked-api/controllers/problem_controller.js
+++ backend/mocked-api/controllers/problem_controller.js
@@ -3,46 +3,46 @@ const httpStatusCode = require('../constants/http-status-code.json');
 const sort = require('../util/sort');
 const _ = require('lodash');
 
-module.exports.registerProblem = async (req, res) =>{
-    try{
+module.exports.registerProblem = async (req, res) => {
+    try {
         const problem = new Problem(req.body.problem);
         console.log(problem);
         await problem.save();
         return res.status(httpStatusCode.CREATED).send(problem);
-        }catch(error){
+        } catch(error) {
             console.log(error);
             return res.status(httpStatusCode.BAD_REQUEST).send({error: "error registering problem"});
         }
 }
 
-module.exports.findProblemById = async (req, res) =>{
+module.exports.findProblemById = async (req, res) => {
     const _id = req.params._id;
-    try{
+    try {
         const problem = await Problem.findOne({_id}); 
         console.log(problem);   
         return res.status(httpStatusCode.OK).send({problem});
-    }catch(err){
+    }catch(err) {
         return res.status(httpStatusCode.NOT_FOUND).send({error: "Problem Not Found"});
     }
 }
 
-module.exports.findOthers = async (req, res) =>{
+module.exports.findOthers = async (req, res) => {
     const level = req.query.level;
     const pattern = req.query.pattern;
     const order = req.query.order;
     const type_question = req.query.type_question;
     let problems = await Problem.find();
-    if(pattern !== undefined){
+    if (pattern !== undefined) {
         problems = await Problem.find({"title": {$regex: `(?i).*${pattern}.*`}});
     }
-    if(level !== undefined){
+    if (level !== undefined) {
         problems = _.filter(problems, p => p.level === parseInt(level));
     }
-    if(type_question !== undefined){
+    if (type_question !== undefined) {
         problems = _.filter(problems, p => p.type_question.includes(type_question));
     }
-    if(order !== undefined){
-        if(order == "invert")
+    if (order !== undefined) {
+        if (order == "invert")
             problems = sort(true, problems);
         else 
             sort(false, problems);
@@ -51,7 +51,6 @@ module.exports.findOthers = async (req, res) =>{
         results: problems,
         message: `${problems.length} results found`
     });
-
 } 
 
 
