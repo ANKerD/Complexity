ad1fd42a2b20da2293274fc650ea3818f5df2199
Anderson Dantas <a.kleber.d@gmail.com>
2019-11-17 18:03:21 -0300

    Fixing #33

--- backend/mocked-api/controllers/ranking_controller.js
+++ backend/mocked-api/controllers/ranking_controller.js
@@ -1,18 +1,24 @@
 const httpStatusCode = require("../constants/http-status-code.json");
-const Player = require('../models/Player');
+const Player = require("../models/Player");
 const _ = require("lodash");
 
 module.exports.listRanking = async (req, res) => {
-    let {param, order} = req.query;
-    let ranking = await Player.find();
-    let profiles = _.forEach(ranking, (p) => {return p.toProfile()});
-    if(order != "asc")
-        order = "desc";
-    if(param == "solved")
-        profiles = _.orderBy(profiles, (user)=> {return _.size(user.problemsSolved)}, order);
-    else
-        profiles = _.orderBy(profiles, ["xp"], order);
-    return res.status(httpStatusCode.OK).send({
-        results: profiles
-    });
-}
+  let { param, order } = req.query;
+  let ranking = await Player.find();
+  let profiles = _.map(ranking, p => {
+    return p.toProfile();
+  });
+  if (order != "asc") order = "desc";
+  if (param == "solved")
+    profiles = _.orderBy(
+      profiles,
+      user => {
+        return _.size(user.problemsSolved);
+      },
+      order
+    );
+  else profiles = _.orderBy(profiles, ["xp"], order);
+  return res.status(httpStatusCode.OK).send({
+    results: profiles
+  });
+};
--- backend/mocked-api/routes/rankingRoute.js
+++ backend/mocked-api/routes/rankingRoute.js
@@ -3,10 +3,9 @@ ranking_controller = require("../controllers/ranking_controller");
 
 const router = Router();
 
-const routes = ()=>{
-
-    router.get('', ranking_controller.listRanking);
-    return router;
-}
+const routes = () => {
+  router.get("/", ranking_controller.listRanking);
+  return router;
+};
 
 module.exports = routes;
